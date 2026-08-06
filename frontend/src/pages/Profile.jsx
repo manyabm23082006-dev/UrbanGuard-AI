25f34a8dc30a3fc81d3c1aa42c371b2de1032441
Manya B M <manyabm23082006@gmail.com>
2026-08-06 11:56:50 +0530

    Create frontend architecture and page structure

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-950 text-white">
+      <h1 className="text-5xl font-bold">Profile Page</h1>
+    </div>
+  );
+}
